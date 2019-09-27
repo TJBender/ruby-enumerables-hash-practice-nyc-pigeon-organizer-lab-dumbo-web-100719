4167a573ac9c2567fb711425d5e8ee9cf4d8ccaf
Tiana Bender <tiana.bender01@gmail.com>
2019-09-27 05:19:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   data.each do |first_level, all_other|
     all_other.each do |category, array|
       array.each do |name|
-        final[name] = {:color => [], :gender => [], :lives => []}
+        final_hash[name] = {:color => [], :gender => [], :lives => []}
       end 
     end 
   end 
